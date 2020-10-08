83537b27f6fb770a6f707b58f1ba0f2898d7e928
Coubiac <benoit.grisot@gmail.com>
2020-10-08 08:39:01 +0200

    correction test integration

--- myerp-business/src/test-business/java/com/dummy/myerp/testbusiness/business/ComptabiliteManagerImplIntegrationTest.java
+++ myerp-business/src/test-business/java/com/dummy/myerp/testbusiness/business/ComptabiliteManagerImplIntegrationTest.java
@@ -137,7 +137,6 @@ public class ComptabiliteManagerImplIntegrationTest extends BusinessTestCase {
 
     @Test
     public void checkAddReferenceTest() throws NotFoundException {
-        vEcritureComptable.setReference(null); // On supprime la référence avant de tester l'ajout
         manager.addReference(vEcritureComptable);
 
         assertThat(vEcritureComptable.getReference()).isEqualTo("AC-2020/00001");
